ec0aefbaa210cfce47a5091c4abd7f01c14205a2
Robert Xiu <xiurobert@gmail.com>
2019-09-04 11:35:21 +0800

    Added interface to base NMS classes on

--- versionsupport/src/main/java/net/derpz/tellrawlib/NMSJsonPacketMsg.java
+++ versionsupport/src/main/java/net/derpz/tellrawlib/NMSJsonPacketMsg.java
@@ -1,4 +1,14 @@
 package net.derpz.tellrawlib;
 
+import org.bukkit.entity.Player;
+
 public interface NMSJsonPacketMsg {
+    /**
+     * Sends a JSON message
+     * You should not be accessing this directly.
+     * Use the helper functions in {@link net.derpz.tellrawlib.core.MessageComponent}
+     * @param p Player to send the message to. Must be castable to CraftPlayer
+     * @param m JSON string which will be used as the message
+     */
+    public void sendMsg(Player p, String m);
 }
