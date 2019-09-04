9eb76f6fbaeb98ee41b6174ee7b898df8741aa3f
Robert Xiu <xiurobert@gmail.com>
2019-09-04 13:37:02 +0800

    Added Hover event

--- api/src/main/java/net/derpz/tellrawlib/core/events/HoverEvent.java
+++ api/src/main/java/net/derpz/tellrawlib/core/events/HoverEvent.java
@@ -9,11 +9,28 @@ public interface HoverEvent {
      */
     enum Action {
         /**
-         * Shows a text component
+         * Shows a {@link net.derpz.tellrawlib.core.components.MessageComponentPrimitive}
          */
         SHOW_TEXT,
+        /**
+         * Shows an {@link org.bukkit.inventory.ItemStack}
+         */
         SHOW_ITEM,
+        /**
+         * Shows an {@link org.bukkit.entity.Entity}
+         */
         SHOW_ENTITY,
+        /**
+         * Shows an {@link org.bukkit.Achievement}
+         * or an {@link org.bukkit.advancement.Advancement}
+         */
         SHOW_ACHIEVEMENT
     }
+
+    /**
+     * Constructs a new HoverEvent with an action and a value
+     * @param action The action you would like to execute
+     * @param value The value that you supply to have the action executed
+     */
+    public void construct(Action action, Object value);
 }
